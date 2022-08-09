caeba33301e5a2720fd6b3453f7c6a788c496876
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-08-09 20:03:58 +0400

    add urls_checks route

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, abort, flash, render_template
 from flask import request, redirect, url_for
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime  # noqa: E501
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, ForeignKey  # noqa: E501
 from urllib.parse import urlparse
 
 load_dotenv()
@@ -31,6 +31,18 @@ urls_table = Table(
     Column('name', String(255), unique=True),
     Column('created_at', DateTime, default=datetime.now),
 )
+
+urls_checks = Table(
+    'urls_checks', meta,
+    Column('id', Integer, primary_key=True),
+    Column('url_id', Integer, ForeignKey('urls.id', ondelete='CASCADE', onupdate='CASCADE')),  # noqa: E501
+    Column('status_code', Integer),
+    Column('h1', String(255)),
+    Column('title', String(255)),
+    Column('description', String(255)),
+    Column('created_at', DateTime, default=datetime.now),
+)
+
 meta.create_all(engine)
 
 
@@ -45,6 +57,11 @@ def validate_url(url):
     return True
 
 
+def normalize(url):
+    o = urlparse(url)
+    return f'{o.scheme}://{o.netloc}'
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -54,15 +71,18 @@ def index():
             flash('Некорректный URL', 'danger')
             return redirect(url_for('index'))
 
+        normalized_url = normalize(url)
         with engine.begin() as connection:
             existed_url = connection.execute(
-                urls_table.select().where(urls_table.c.name == url)).first()
+                urls_table.select().where(
+                    urls_table.c.name == normalized_url)
+            ).first()
             if existed_url:
                 id = existed_url.id
                 flash('Страница уже существует', 'info')
             else:
                 new_url = connection.execute(
-                    urls_table.insert().values(name=url))
+                    urls_table.insert().values(name=normalized_url))
                 id = new_url.inserted_primary_key[0]
             return redirect(url_for('get_url', id=id))
 
@@ -82,11 +102,26 @@ def get_url(id):
         url = connection.execute(
             urls_table.select().where(urls_table.c.id == id)
         ).first()
-    if url is None:
-        abort(404)
-    return render_template('urls/url.html', url=url)
+        if url is None:
+            abort(404)
+        checks = connection.execute(
+            urls_checks.select().where(urls_checks.c.url_id == id)
+        )
+
+    return render_template('urls/url.html', url=url, checks=checks)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
+
+
+@app.route('/urls/<int:id>/checks', methods=['POST'])
+def url_checks(id):
+    with engine.begin() as connection:
+        connection.execute(
+            urls_checks.insert().values(
+                url_id=id,
+                created_at=datetime.now(),
+            ))
+    return redirect(url_for('get_url', id=id))
