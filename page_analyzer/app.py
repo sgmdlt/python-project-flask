170e70e1e1a0aa89605061dfc95ce2cc333d97b3
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-08-10 20:03:55 +0400

    add status_code info

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from tabnanny import check
+from time import strftime
 from dotenv import load_dotenv
 import os
+import requests
 from flask import Flask, abort, flash, render_template
 from flask import request, redirect, url_for
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, ForeignKey  # noqa: E501
+from sqlalchemy import create_engine, desc, MetaData, Table, Column, Integer, String, DateTime, ForeignKey  # noqa: E501
 from urllib.parse import urlparse
 
 load_dotenv()
@@ -62,6 +65,11 @@ def normalize(url):
     return f'{o.scheme}://{o.netloc}'
 
 
+@app.template_filter()
+def format_datetime(value):
+    return strftime('%Y-%m-%d %H:%M:%S', value.timetuple())
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -74,12 +82,12 @@ def index():
         normalized_url = normalize(url)
         with engine.begin() as connection:
             existed_url = connection.execute(
-                urls_table.select().where(
-                    urls_table.c.name == normalized_url)
-            ).first()
+                urls_table.select().
+                where(urls_table.c.name == normalized_url)).first()
             if existed_url:
                 id = existed_url.id
                 flash('Страница уже существует', 'info')
+
             else:
                 new_url = connection.execute(
                     urls_table.insert().values(name=normalized_url))
@@ -91,22 +99,30 @@ def index():
 
 @app.route('/urls')
 def urls():
-    with engine.begin() as connection:
-        url_list = connection.execute(urls_table.select())
-    return render_template('urls/index.html', urls=url_list)
+    with engine.begin() as conn:
+        url_list = conn.execute(urls_table.select().order_by('id'))
+        checks = conn.execute(
+            urls_checks.select().
+            distinct('url_id').
+            order_by('url_id').
+            order_by(desc('created_at'))
+        )
+
+    return render_template('urls/index.html', urls = zip(url_list, checks))
 
 
 @app.route('/urls/<int:id>')
 def get_url(id):
     with engine.begin() as connection:
         url = connection.execute(
-            urls_table.select().where(urls_table.c.id == id)
-        ).first()
+            urls_table.select().
+            where(urls_table.c.id == id)).first()
         if url is None:
             abort(404)
         checks = connection.execute(
             urls_checks.select().where(urls_checks.c.url_id == id)
         )
+        checks = sorted(checks, key=lambda x: x.created_at, reverse=True)
 
     return render_template('urls/url.html', url=url, checks=checks)
 
@@ -119,9 +135,17 @@ def page_not_found(error):
 @app.route('/urls/<int:id>/checks', methods=['POST'])
 def url_checks(id):
     with engine.begin() as connection:
+        url = connection.execute(
+            urls_table.select().
+            where(urls_table.c.id == id)).first()
+        response = requests.get(url.name)
+
         connection.execute(
             urls_checks.insert().values(
                 url_id=id,
+                status_code=response.status_code,
+                h1=response.headers.get('h1'),
+                title=response.headers.get('title'),
                 created_at=datetime.now(),
             ))
     return redirect(url_for('get_url', id=id))
